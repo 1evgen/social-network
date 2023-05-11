a8b8476d1e7e753093c58eb331e2082990df47a0
Evgenii <evgusa83@gmail.com>
2023-05-11 17:11:54 +0300

    create folder redux

---
+++ src/Redux/State.ts
@@ -0,0 +1,28 @@
+
+export const state = {
+    dialog: {
+        dialogsData:  [
+            {id:1, name: "Ward" },
+            {id:2, name: "Maddison" },
+            {id:3, name: "Tielemans" },
+            {id:4, name: "Ndidi" },
+            {id:5, name: "Daka" },
+        ],
+
+        messagesData: [
+            {id: 1, message: 'Hi' },
+            {id: 2, message: 'Hello' },
+            {id: 3, message: 'How are you' },
+            {id: 4, message: 'fine' },
+            {id: 5, message: 'Yo' },
+        ],
+    },
+    post: {
+        postData: [
+            {id: 1, message: 'Hi how are you', likesCount: 15 },
+            {id: 2, message: 'Hi, i\'m fine thanks',likesCount: 5 },
+        ],
+
+    }
+
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,35 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {state} from './Redux/State'
 
-const dialogsData = [
-    {id:1, name: "Ward" },
-    {id:2, name: "Maddison" },
-    {id:3, name: "Tielemans" },
-    {id:4, name: "Ndidi" },
-    {id:5, name: "Daka" },
-]
-
-const messagesData = [
-    {id: 1, message: 'Hi' },
-    {id: 2, message: 'Hello' },
-    {id: 3, message: 'How are you' },
-    {id: 4, message: 'fine' },
-    {id: 5, message: 'Yo' },
-]
-
-
-const postData = [
-    {id: 1, message: 'Hi how are you', likesCount: 15 },
-    {id: 2, message: 'Hi, i\'m fine thanks',likesCount: 5 },
-]
 
 
 ReactDOM.render(
-    <App dialogs={dialogsData}
-         messages={messagesData}
-         myPosts={postData}
-
+    <App dialogs={state.dialog.dialogsData}
+         messages={state.dialog.messagesData}
+         myPosts={state.post.postData}
 
     />,
     document.getElementById('root')
